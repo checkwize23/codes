2bdc0b7df547ebace363d857542899e1874b0639
Rohith <checkwize23@gmail.com>
2026-09-09 15:18:28 +0530

    altered careertab and altered its function in superadmin

--- frontend/src/components/SuperAdminDashboard.jsx
+++ frontend/src/components/SuperAdminDashboard.jsx
@@ -1837,7 +1837,7 @@ const confirmConsentDelete = async () => {
       </div>
       )}  
       {activeTab === 'careers' &&(
-        <div className="w-full px-4 py-2 bg-gradient-to-r from-blue-300 to-purple-400 text-white font-semibold rounded-lg hover:from-blue-400 hover:to-purple-500 transition-all duration-200">
+        <div className="w-full px-4 py-2 bg-gradient-to-r from-blue-400 to-purple-500 text-white font-semibold rounded-lg transition-all duration-200">
           <CareersTab />
         </div>  
       )}
